7fd01216c1d2fbeb029b49b7fe631c9bdbbcb831
kazuki takahashi <tkhsh.schoen@gmail.com>
2019-04-09 23:29:49 +0900

    add edge visualization

--- scripts/node_edge_map_manager.py
+++ scripts/node_edge_map_manager.py
@@ -9,8 +9,9 @@ import pprint
 
 import rospy
 import tf
+from std_msgs.msg import ColorRGBA
 from nav_msgs.msg import Odometry
-from geometry_msgs.msg import Quaternion
+from geometry_msgs.msg import Quaternion, Point
 from visualization_msgs.msg import Marker, MarkerArray
 
 np.set_printoptions(linewidth=200)
@@ -26,7 +27,9 @@ class NodeEdgeMapManager:
         pprint.pprint(self.map_data)
 
         self.node_marker = MarkerArray()
-        self.node_marker_pub = rospy.Publisher('/node_edge_map/node/viz', MarkerArray, queue_size=1, latch=True)
+        self.node_marker_pub = rospy.Publisher('/node_edge_map/viz/node', MarkerArray, queue_size=1, latch=True)
+        self.edge_marker = Marker()
+        self.edge_marker_pub = rospy.Publisher('/node_edge_map/viz/edge', Marker, queue_size=1, latch=True)
         self.lock = threading.Lock()
 
         print '=== node edge map manager ==='
@@ -34,8 +37,10 @@ class NodeEdgeMapManager:
     def process(self):
         r = rospy.Rate(10)
         self.make_node_marker()
+        self.make_edge_marker()
         while not rospy.is_shutdown():
             self.node_marker_pub.publish(self.node_marker)
+            self.edge_marker_pub.publish(self.edge_marker)
             r.sleep()
 
     def make_node_marker(self):
@@ -49,17 +54,52 @@ class NodeEdgeMapManager:
             n.action = Marker().ADD
             n.type = Marker().CUBE
             n.lifetime = rospy.Duration()
-            n.scale.x = 0.5
-            n.scale.y = 0.5
-            n.scale.z = 0.5
-            n.color.r = 0.0
-            n.color.g = 1.0
-            n.color.b = 0.0
-            n.color.a = 0.7
-            n.pose.position.x = node['point']['x']
-            n.pose.position.y = node['point']['y']
+            self.set_marker_scale(n, 0.5, 0.5, 0.5)
+            self.set_marker_rgb(n, 0.0, 1.0, 0.0)
+            self.set_marker_position(n, node['point']['x'], node['point']['y'], 0)
             self.node_marker.markers.append(n)
-            print node
+
+    def make_edge_marker(self):
+        self.edge_marker = Marker()
+        self.edge_marker.header.frame_id = self.map_data['MAP_FRAME']
+        self.edge_marker.header.stamp = rospy.get_rostime()
+        self.edge_marker.id = 0
+        self.edge_marker.action = Marker().ADD
+        self.edge_marker.type = Marker().LINE_LIST
+        self.edge_marker.lifetime = rospy.Duration()
+        self.edge_marker.scale.x = 0.3
+        for edge in self.map_data['EDGE']:
+            p0 = Point()
+            p0.x = self.map_data['NODE'][edge['node_id'][0]]['point']['x']
+            p0.y = self.map_data['NODE'][edge['node_id'][0]]['point']['y']
+            p1 = Point()
+            p1.x = self.map_data['NODE'][edge['node_id'][1]]['point']['x']
+            p1.y = self.map_data['NODE'][edge['node_id'][1]]['point']['y']
+            self.edge_marker.points.append(p0)
+            self.edge_marker.points.append(p1)
+            color = ColorRGBA()
+            color.r = 0.0
+            color.g = 0.0
+            color.b = 1.0
+            color.a = 0.7
+            self.edge_marker.colors.append(color)
+            self.edge_marker.colors.append(color)
+
+    def set_marker_scale(self, marker, x, y, z):
+        marker.scale.x = x
+        marker.scale.y = y
+        marker.scale.z = z
+
+    def set_marker_position(self, marker, x, y, z):
+        marker.pose.position.x = x
+        marker.pose.position.y = y
+        marker.pose.position.z = z
+
+    def set_marker_rgb(self, marker, r, g, b, a=1.7):
+        marker.color.r = r
+        marker.color.g = g
+        marker.color.b = b
+        marker.color.a = a
 
 if __name__ == '__main__':
     node_edge_map_manager = NodeEdgeMapManager()
