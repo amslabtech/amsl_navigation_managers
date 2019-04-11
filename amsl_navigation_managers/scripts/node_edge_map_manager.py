d6792ee4c387c60fb302e279e3dbd655f5563b80
kazuki takahashi <tkhsh.schoen@gmail.com>
2019-04-11 13:09:00 +0900

    close #5

--- amsl_navigation_managers/scripts/node_edge_map_manager.py
+++ amsl_navigation_managers/scripts/node_edge_map_manager.py
@@ -14,6 +14,8 @@ from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Quaternion, Point
 from visualization_msgs.msg import Marker, MarkerArray
 
+from amsl_navigation_msgs.msg import Node, Edge, NodeEdgeMap
+
 np.set_printoptions(linewidth=200)
 
 class NodeEdgeMapManager:
@@ -32,6 +34,10 @@ class NodeEdgeMapManager:
         self.edge_marker_pub = rospy.Publisher('/node_edge_map/viz/edge', Marker, queue_size=1, latch=True)
         self.id_marker = MarkerArray()
         self.id_marker_pub = rospy.Publisher('/node_edge_map/viz/id', MarkerArray, queue_size=1, latch=True)
+
+        self.node_edge_map = NodeEdgeMap()
+        self.node_edge_map_pub = rospy.Publisher('/node_edge_map', NodeEdgeMap, queue_size=1, latch=True)
+
         self.lock = threading.Lock()
 
         print '=== node edge map manager ==='
@@ -41,10 +47,12 @@ class NodeEdgeMapManager:
         self.make_node_marker()
         self.make_edge_marker()
         self.make_id_marker()
+        self.make_node_edge_map()
+        self.node_marker_pub.publish(self.node_marker)
+        self.edge_marker_pub.publish(self.edge_marker)
+        self.id_marker_pub.publish(self.id_marker)
+        self.node_edge_map_pub.publish(self.node_edge_map)
         while not rospy.is_shutdown():
-            self.node_marker_pub.publish(self.node_marker)
-            self.edge_marker_pub.publish(self.edge_marker)
-            self.id_marker_pub.publish(self.id_marker)
             r.sleep()
 
     def make_node_marker(self):
@@ -124,6 +132,31 @@ class NodeEdgeMapManager:
         marker.color.g = g
         marker.color.b = b
         marker.color.a = a
+
+    def make_node_edge_map(self):
+        time = rospy.get_rostime()
+        self.node_edge_map.header.stamp = time
+        self.node_edge_map.header.frame_id = self.map_data['MAP_FRAME'] 
+        self.node_edge_map.nodes = [] 
+        self.node_edge_map.edges = []
+        for node in self.map_data['NODE']:
+            n = Node()
+            n.type = node['type']
+            n.label = node['label']
+            n.id = node['id']
+            n.point.x = node['point']['x']
+            n.point.y = node['point']['y']
+            self.node_edge_map.nodes.append(n)
+        for edge in self.map_data['EDGE']:
+            e = Edge()
+            x0 = self.map_data['NODE'][edge['node_id'][0]]['point']['x']
+            y0 = self.map_data['NODE'][edge['node_id'][0]]['point']['y']
+            x1 = self.map_data['NODE'][edge['node_id'][1]]['point']['x']
+            y1 = self.map_data['NODE'][edge['node_id'][1]]['point']['y']
+            e.length = math.sqrt((x0 - x1) ** 2 + (y0 - y1) ** 2)
+            e.node0_id = edge['node_id'][0]
+            e.node1_id = edge['node_id'][1]
+            self.node_edge_map.edges.append(e)
 
 if __name__ == '__main__':
     node_edge_map_manager = NodeEdgeMapManager()
