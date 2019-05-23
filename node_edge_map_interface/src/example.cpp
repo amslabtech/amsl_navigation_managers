d3214155419151fb50b6aadebc13289be983e35f
kazuki takahashi <tkhsh.schoen@gmail.com>
2019-05-23 23:05:18 +0900

    add example.cpp

---
+++ node_edge_map_interface/src/example.cpp
@@ -0,0 +1,12 @@
+#include <ros/ros.h>
+
+#include "node_edge_map_interface/node_edge_map_interface.h"
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "node_edge_map_interface_example");
+    while(ros::ok()){
+
+    }
+    return 0;
+}
