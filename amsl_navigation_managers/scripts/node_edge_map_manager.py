f442b97dd5f05bb18ca403b09cab29395fdac799
kazuki takahashi <tkhsh.schoen@gmail.com>
2019-09-03 12:06:40 +0900

    add origin_utm

--- amsl_navigation_managers/scripts/node_edge_map_manager.py
+++ amsl_navigation_managers/scripts/node_edge_map_manager.py
@@ -207,6 +207,8 @@ class NodeEdgeMapManager:
         time = rospy.get_rostime()
         self.node_edge_map.header.stamp = time
         self.node_edge_map.header.frame_id = self.map_data['MAP_FRAME']
+        self.node_edge_map.origin_utm.x = self.map_data['ORIGIN_UTM']['x']
+        self.node_edge_map.origin_utm.y = self.map_data['ORIGIN_UTM']['y']
         self.node_edge_map.nodes = []
         self.node_edge_map.edges = []
         for node in self.map_data['NODE']:
