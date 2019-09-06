d681709f370c7ef75f08ceae5dc9f27d3035681a
kazuki takahashi <tkhsh.schoen@gmail.com>
2019-09-06 15:41:39 +0900

    private -> protected

--- amsl_navigation_managers/include/amsl_navigation_managers/node_edge_map_interface.h
+++ amsl_navigation_managers/include/amsl_navigation_managers/node_edge_map_interface.h
@@ -8,17 +8,19 @@
 class NodeEdgeMapInterface
 {
 public:
-	NodeEdgeMapInterface(void);
+    NodeEdgeMapInterface(void);
 
-	void set_map(amsl_navigation_msgs::NodeEdgeMap&);
-	void get_node_from_id(int id, amsl_navigation_msgs::Node&);
-	void get_edge_from_node_id(int, int, amsl_navigation_msgs::Edge&);
-	int get_node_index_from_id(int);
-	int get_edge_index_from_node_id(int, int);
-	amsl_navigation_msgs::Edge get_edge_from_index(int);
+    void set_map(amsl_navigation_msgs::NodeEdgeMap&);
+    void get_node_from_id(int id, amsl_navigation_msgs::Node&);
+    void get_edge_from_node_id(int, int, amsl_navigation_msgs::Edge&);
+    int get_node_index_from_id(int);
+    int get_edge_index_from_node_id(int, int);
+    amsl_navigation_msgs::Edge get_edge_from_index(int);
+    std::string get_map_header_frame_id(void);
+    int get_edge_num(void);
 
-private:
-	amsl_navigation_msgs::NodeEdgeMap map;
+protected:
+    amsl_navigation_msgs::NodeEdgeMap map;
 };
 
 #endif// __NODE_EDGE_MAP_INTERFACE_H
--- amsl_navigation_managers/src/node_edge_map_interface.cpp
+++ amsl_navigation_managers/src/node_edge_map_interface.cpp
@@ -7,53 +7,63 @@ NodeEdgeMapInterface::NodeEdgeMapInterface(void)
 
 void NodeEdgeMapInterface::set_map(amsl_navigation_msgs::NodeEdgeMap& _map)
 {
-	map = _map;
+    map = _map;
 }
 
 void NodeEdgeMapInterface::get_node_from_id(int id, amsl_navigation_msgs::Node& node)
 {
-	for(auto n : map.nodes){
-		if(n.id == id){
-			node = n;
-			return;
-		}
-	}
+    for(auto n : map.nodes){
+        if(n.id == id){
+            node = n;
+            return;
+        }
+    }
 }
 
 void NodeEdgeMapInterface::get_edge_from_node_id(int node0_id, int node1_id, amsl_navigation_msgs::Edge& edge)
 {
-	for(auto e : map.edges){
-		if(e.node0_id == node0_id && e.node1_id == node1_id){
-			edge = e;
-			return;
-		}
-	}
+    for(auto e : map.edges){
+        if(e.node0_id == node0_id && e.node1_id == node1_id){
+            edge = e;
+            return;
+        }
+    }
 }
 
 int NodeEdgeMapInterface::get_edge_index_from_node_id(int node0_id, int node1_id)
 {
-	int index = 0;
-	for(auto e : map.edges){
-		if(e.node0_id == node0_id && e.node1_id == node1_id){
-			return index;
-		}
-		index++;
-	}
+    int index = 0;
+    for(auto e : map.edges){
+        if(e.node0_id == node0_id && e.node1_id == node1_id){
+            return index;
+        }
+        index++;
+    }
 }
 
 int NodeEdgeMapInterface::get_node_index_from_id(int id)
 {
-	int i = 0;
-	for(auto n : map.nodes){
-		if(n.id == id){
-			return i;
-		}
-		i++;
-	}
-	return -1;
+    int i = 0;
+    for(auto n : map.nodes){
+        if(n.id == id){
+            return i;
+        }
+        i++;
+    }
+    return -1;
 }
 
 amsl_navigation_msgs::Edge NodeEdgeMapInterface::get_edge_from_index(int edge_index)
 {
-	return map.edges[edge_index];
+    return map.edges[edge_index];
+}
+
+std::string NodeEdgeMapInterface::get_map_header_frame_id(void)
+{
+    return map.header.frame_id;
+}
+
+int NodeEdgeMapInterface::get_edge_num(void)
+{
+    return map.edges.size();
 }
