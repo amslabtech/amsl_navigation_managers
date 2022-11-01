f6077a67887a1a0c61f84571605ea0f8d5114ec8
Kazutoshi-Fukaya <kazutoshi4423@gmail.com>
2022-11-01 17:23:46 +0900

    debug

--- amsl_navigation_managers/scripts/rwrc22_task_manager.py
+++ amsl_navigation_managers/scripts/rwrc22_task_manager.py
@@ -5,7 +5,7 @@
 import yaml
 
 import rospy
-from std_msgs.msg import Bool, Int32MultiArray
+from std_msgs.msg import Bool, Int32
 
 class TaskManager:
     def __init__(self):
@@ -14,21 +14,24 @@ class TaskManager:
 
         self.TASK_LIST_PATH = rospy.get_param('~TASK_LIST_PATH')
 
-        self.current_checkpoint_id_sub = rospy.Subscriber('/current_checkpoint', Int32MultiArray, self.checkpoint_id_callback)
+        self.current_checkpoint_id_sub = rospy.Subscriber('/current_checkpoint', Int32, self.checkpoint_id_callback)
 
         self.detect_line_flag_pub = rospy.Publisher('/detect_line', Bool, queue_size=1)
 
-        self.task_data = self.load_task_from_yaml()
         self.get_task = False
         self.last_checkpoint_id = -1
         self.current_checkpoint_id= -1
         self.detect_line_flag = False
+        self.task_data = self.load_task_from_yaml()
 
     def process(self):
         r = rospy.Rate(10)
         while not rospy.is_shutdown():
+            print(self.get_task)
             task_type = self.search_task_from_node_id(self.last_checkpoint_id, self.current_checkpoint_id)
-            if task_type == "detect_line":
+            # print('node0_id =', self.last_checkpoint_id, 'node1_id = ', self.current_checkpoint_id)
+            print('task_type = %s' % task_type)
+            if task_type == 'detect_line':
                 self.detect_line_flag = True
             else:
                 self.detect_line_flag = False
@@ -39,21 +42,24 @@ class TaskManager:
         with open(self.TASK_LIST_PATH) as file:
             task_data = yaml.safe_load(file)
             self.get_task = True
+            print('get task')
         return task_data
 
     def checkpoint_id_callback(self, checkpoint_id):
         self.last_checkpoint_id = self.current_checkpoint_id
-        self.current_checkpoint_id = checkpoint_id
+        self.current_checkpoint_id = int(checkpoint_id.data)
 
     def search_task_from_node_id(self, node0_id, node1_id):
         if self.get_task == True:
-            for task in enumerate(self.task_data['task']):
+            # print('get_task is true')
+            for count, task in enumerate(self.task_data['task']):
+                # print(task)
                 if (task['edge']['node0_id'] == node0_id) and (task['edge']['node1_id'] == node1_id):
                     return task['task_type']
             else:
-                return ""
+                return ''
         else:
-            return ""
+            return ''
 
 if __name__ == '__main__':
     task_manager = TaskManager()
