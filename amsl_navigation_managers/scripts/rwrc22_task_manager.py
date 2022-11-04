b296791add6be5bc32dbdb45754a6d498a5c2921
Kazutoshi-Fukaya <kazutoshi4423@gmail.com>
2022-11-04 15:25:23 +0900

    stop_line_detectorからtopicを受け取ってmotion_decisionへ停止信号をpublishする機能を追加 受け取るtopic名は未定

--- amsl_navigation_managers/scripts/rwrc22_task_manager.py
+++ amsl_navigation_managers/scripts/rwrc22_task_manager.py
@@ -15,14 +15,18 @@ class TaskManager:
         self.TASK_LIST_PATH = rospy.get_param('~TASK_LIST_PATH')
 
         self.current_checkpoint_id_sub = rospy.Subscriber('/current_checkpoint', Int32, self.checkpoint_id_callback)
+        self.is_detected_sub = rospy.Subscriber('/hoge', Bool, self.detected_line_callback)
 
         self.detect_line_flag_pub = rospy.Publisher('/detect_line', Bool, queue_size=1)
+        self.task_stop_flag_pub = rospy.Publisher('/task/stop' , Bool, queue_size=1)
 
         self.get_task = False
         self.last_checkpoint_id = -1
         self.current_checkpoint_id= -1
         self.detect_line_flag = False
         self.task_data = self.load_task_from_yaml()
+
+        self.last_line_flag = False
 
     def process(self):
         r = rospy.Rate(10)
@@ -49,6 +53,14 @@ class TaskManager:
         if self.current_checkpoint_id != int(checkpoint_id.data):
             self.last_checkpoint_id = self.current_checkpoint_id
             self.current_checkpoint_id = int(checkpoint_id.data)
+
+    def detected_line_callback(self, flag):
+        # print('current flag = ', flag.data, 'last flag = ', self.last_line_flag)
+        if self.last_line_flag == True and flag.data == False:
+            self.task_stop_flag_pub.publish(False)
+        if self.last_line_flag == False and flag.data == True:
+            self.task_stop_flag_pub.publish(True)
+        self.last_line_flag = flag.data
 
     def search_task_from_node_id(self, node0_id, node1_id):
         if self.get_task == True:
