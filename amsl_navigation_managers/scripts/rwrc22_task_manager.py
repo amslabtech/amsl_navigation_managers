0c040e670fa95d59bc7c5fcd748acca60e05a1f8
morisitakanta <morisitakanta@gmail.com>
2022-11-09 17:35:05 +0900

    add subs & pubs turn when waypoint updated

--- amsl_navigation_managers/scripts/rwrc22_task_manager.py
+++ amsl_navigation_managers/scripts/rwrc22_task_manager.py
@@ -3,9 +3,13 @@
 
 #注：rwrc21_task_managerとは仕様が全く違います
 import yaml
+import math
 
 import rospy
 from std_msgs.msg import Bool, Int32
+from geometry_msgs.msg import Twist, PoseStamped, PoseWithCovarianceStamped
+from sensor_msgs.msg import Joy
+import tf2_ros
 
 class TaskManager:
     def __init__(self):
@@ -14,33 +18,74 @@ class TaskManager:
 
         self.TASK_LIST_PATH = rospy.get_param('~TASK_LIST_PATH')
 
+        # ros
         self.current_checkpoint_id_sub = rospy.Subscriber('/current_checkpoint', Int32, self.checkpoint_id_callback)
         self.stop_line_flag_sub = rospy.Subscriber('/stop_line_flag', Bool, self.stop_line_flag_callback)
+        self.local_planner_cmd_vel_sub = rospy.Subscriber('/local_planner/cmd_vel', Twist, self.local_planner_cmd_vel_callback)
+        self.local_goal_sub = rospy.Subscriber('/local_goal', PoseStamped, self.local_goal_callback)
+        self.joy_sub = rospy.Subscriber('/joy', Joy, self.joy_callback)
+        self.amcl_pose_sub = rospy.Subscriber('/amcl_pose', PoseWithCovarianceStamped, self.amcl_pose_callback)
 
         self.detect_line_flag_pub = rospy.Publisher('/detect_line', Bool, queue_size=1)
-        self.task_stop_flag_pub = rospy.Publisher('/task/stop' , Bool, queue_size=1)
+        # self.task_stop_flag_pub = rospy.Publisher('/task/stop' , Bool, queue_size=1)
+        self.cmd_vel_pub = rospy.Publisher('/local_path/cmd_vel', Twist, queue_size=1)
 
+        # params in callback function
+        self.current_checkpoint_id = self.last_checkpoint_id = -1
+        self.stop_line_flag = False
+        self.local_planner_cmd_vel = Twist()
+        self.local_goal = PoseStamped()
+        self.joy = Joy()
+        self.amcl_pose = PoseWithCovarianceStamped()
+
+        # params
         self.get_task = False
-        self.last_checkpoint_id = -1
-        self.current_checkpoint_id= -1
-        self.detect_line_flag = False
         self.task_data = self.load_task_from_yaml()
-
         self.last_line_flag = False
 
     def process(self):
+        print('=== process started ===')
+        if self.get_task == False:
+            rospy.logerr('task is not get')
+            exit(-1)
+
         r = rospy.Rate(10)
         while not rospy.is_shutdown():
-            # print(self.get_task)
+            rospy.loginfo('================================================================')
             task_type = self.search_task_from_node_id(self.last_checkpoint_id, self.current_checkpoint_id)
-            # print('node0_id =', self.last_checkpoint_id, 'node1_id = ', self.current_checkpoint_id)
-            # print('task_type = %s' % task_type)
+
+            ##### white line detector & stop behind robot #####
+            enable_detect_line = Bool()
             if task_type == 'detect_line':
-                self.detect_line_flag = True
+                enable_detect_line.data = True
             else:
-                self.detect_line_flag = False
-            self.detect_line_flag_pub.publish(self.detect_line_flag)
+                enable_detect_line.data = False
+            self.detect_line_flag_pub.publish(enable_detect_line)
+            rospy.loginfo('detect_line_flag_pub = %s' % enable_detect_line.data)
+            ##### white line detector & stop behind robot #####
+
+            cmd_vel = Twist()
+            cmd_vel.linear.x = self.local_planner_cmd_vel.linear.x
+            cmd_vel.angular.z = self.local_planner_cmd_vel.angular.z
+
+            ##### turn at node #####
+            if(self.current_checkpoint_id != self.last_checkpoint_id):
+                cmd_vel = self.get_turn_around_cmd_vel(self.local_goal, self.local_planner_cmd_vel)
+            ##### turn at node #####
+
+            self.cmd_vel_pub.publish(cmd_vel)
+
+            rospy.spin()
             r.sleep()
+
+    def get_turn_around_cmd_vel(self, goal, local_planner_cmd_vel):
+        goal_yaw = math.atan2(goal.pose.position.y, goal.pose.position.x)
+        if(goal_yaw < 0.2):
+            return local_planner_cmd_vel
+        cmd_vel = Twist()
+        cmd_vel.linear.x = 0.0
+        cmd_vel.angular.z = local_planner_cmd_vel.angular.z
+        return cmd_vel
 
     def load_task_from_yaml(self):
         with open(self.TASK_LIST_PATH) as file:
@@ -55,12 +100,19 @@ class TaskManager:
             self.current_checkpoint_id = int(checkpoint_id.data)
 
     def stop_line_flag_callback(self, flag):
-        # print('current flag = ', flag.data, 'last flag = ', self.last_line_flag)
-        if self.last_line_flag == True and flag.data == False:
-            self.task_stop_flag_pub.publish(False)
-        if self.last_line_flag == False and flag.data == True:
-            self.task_stop_flag_pub.publish(True)
-        self.last_line_flag = flag.data
+        self.stop_line_flag = flag.data
+
+    def local_planner_cmd_vel_callback(self, msg):
+        self.local_planner_cmd_vel = msg
+
+    def local_goal_callback(self, msg):
+        self.local_goal = msg
+
+    def joy_callback(self, msg):
+        self.joy = msg
+
+    def amcl_pose_callback(self, msg):
+        self.amcl_pose = msg
 
     def search_task_from_node_id(self, node0_id, node1_id):
         if self.get_task == True:
@@ -69,8 +121,6 @@ class TaskManager:
                 # print(task)
                 if (task['edge']['node0_id'] == node0_id) and (task['edge']['node1_id'] == node1_id):
                     return task['task_type']
-            else:
-                return ''
         else:
             return ''
 
