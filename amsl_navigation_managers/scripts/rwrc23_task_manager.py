bb530f975d97b9237c11dab10aa5e5a9ac012de8
Toshiki Nakamura <tnakamura.amsl@gmail.com>
2024-07-26 12:17:18 +0900

    Use optional

--- amsl_navigation_managers/scripts/rwrc23_task_manager.py
+++ amsl_navigation_managers/scripts/rwrc23_task_manager.py
@@ -58,26 +58,21 @@ class TaskManager:
         self.pfp_finish_flag = rospy.get_param("~pfp_finish_flag", "")
 
         # params in callback function
-        self.current_checkpoint_id = self.next_checkpoint_id = -1
+        self.current_checkpoint_id = None
+        self.next_checkpoint_id = None
         self.target_velocity = Twist()
 
         # params
-        self.get_task = False
         self.task_data = self.load_task_from_yaml()
-        self.reached_checkpoint = False
-        self.get_stop_list = False
         self.stop_list = self.load_stop_list_from_yaml()
-        self.start_announce_flag = False
-        self.announce_pid = 0
-        self.current_planner = ""
-        self.checkpoint_list = Int32MultiArray()
+        self.current_planner = None
+        self.checkpoint_list = None
         self.finish_flag = Bool()
         self.skip_mode_flag = Bool()
         self.recovery_mode_flag = Bool()
 
         # msg update flags
         self.checkpoint_list_subscribed = False
-        self.checkpoint_id_subscribed = False
 
         # ros
         self.current_checkpoint_id_sub = rospy.Subscriber(
@@ -127,136 +122,154 @@ class TaskManager:
         if self.USE_DETECT_WHITE_LINE:
             rospy.wait_for_service("/stop_line_detector/request")
         rospy.wait_for_service("/traffic_light_detector/request")
-        if self.get_task == False or self.get_stop_list == False:
-            rospy.logerr("task or stop list is not loaded")
-            exit(-1)
 
         prev_task_type = "init"
         r = rospy.Rate(10)
         self.target_velocity.linear.x = self.dwa_target_velocity
         while not rospy.is_shutdown():
-            if self.checkpoint_id_subscribed:
-                rospy.loginfo_throttle(1, "=====")
-                task_type = self.search_task_from_node_id(
-                    self.current_checkpoint_id, self.next_checkpoint_id
-                )
-                rospy.loginfo_throttle(1, f"task_type : {task_type}")
-                rospy.loginfo_throttle(1, f"planner : {self.current_planner}")
-                rospy.loginfo_throttle(
-                    1, f"current_checkpoint : {self.current_checkpoint_id}"
-                )
-                rospy.loginfo_throttle(
-                    1, f"next_checkpoint : {self.next_checkpoint_id}"
-                )
-
-                # update task
-                if prev_task_type != task_type:
-                    rospy.logwarn(f"task updated : {task_type}")
-                    # detect_line
-                    if (
-                        task_type == "detect_line"
-                        and self.USE_DETECT_WHITE_LINE
-                    ):
-                        try:
-                            resp = self.stop_line_detector_client(True)
-                            rospy.logwarn(resp.message)
-                        except rospy.ServiceException as e:
-                            rospy.logwarn(e)
-                        self.use_point_follow_planner()
-                        self.target_velocity.linear.x = (
-                            self.detect_line_pfp_target_velocity
-                        )
-                    else:
-                        try:
-                            resp = self.stop_line_detector_client(False)
-                            rospy.logwarn(resp.message)
-                        except rospy.ServiceException as e:
-                            rospy.logwarn(e)
-
-                    # traffic_light
-                    if task_type == "traffic_light":
-                        try:
-                            resp = self.traffic_light_detector_client(True)
-                            rospy.logwarn(resp.message)
-                        except rospy.ServiceException as e:
-                            rospy.logwarn(e)
-                        self.use_dwa_planner()
-                    else:
-                        try:
-                            resp = self.traffic_light_detector_client(False)
-                            rospy.logwarn(resp.message)
-                        except rospy.ServiceException as e:
-                            rospy.logwarn(e)
-
-                    # point_follow_planner
-                    if task_type == "in_line":
-                        self.use_point_follow_planner()
-
-                    # slow
-                    if task_type == "slow":
-                        self.target_velocity.linear.x = (
-                            self.slow_target_velocity
-                        )
-
-                    # no task
-                    if task_type == "":
-                        self.use_dwa_planner()
-
-                    # skip_mode
-                    if task_type == "" and not self.is_stop_node(
-                        self.stop_list, self.next_checkpoint_id
-                    ):
-                        self.skip_mode_flag.data = True
-                    else:
-                        self.skip_mode_flag.data = False
-
-                    # stop node
-                    if self.is_stop_node(
-                        self.stop_list, self.current_checkpoint_id
-                    ):
-                        try:
-                            resp = self.task_stop_client(True)
-                            rospy.logwarn(resp.message)
-                        except rospy.ServiceException as e:
-                            rospy.logwarn(e)
-                        del self.stop_list[0]
-
-                    # recovery_mode
-                    if task_type == "" or task_type == "slow":
-                        self.recovery_mode_flag.data = True
-                    else:
-                        self.recovery_mode_flag.data = False
-
-                # publish
-                self.target_velocity_pub.publish(self.target_velocity)
-                self.finish_flag_pub.publish(self.finish_flag.data)
-                self.skip_mode_flag_pub.publish(self.skip_mode_flag.data)
-                self.recovery_mode_flag_pub.publish(
-                    self.recovery_mode_flag.data
-                )
-
-                if self.finish_flag.data:
-                    rospy.sleep(self.sleep_time_after_finish)
-
-                prev_task_type = task_type
-                self.finish_flag.data = False
-            else:
+            if self.current_checkpoint_id is None:
                 rospy.logwarn_throttle(1, "Checkpoint id is not updated")
+                r.sleep()
+                continue
+            elif self.next_checkpoint_id is None:
+                rospy.logwarn_throttle(1, "Next checkpoint id is not updated")
+                r.sleep()
+                continue
+            elif self.checkpoint_list is None:
+                rospy.logwarn_throttle(1, "Checkpoint list is not updated")
+                r.sleep()
+                continue
+            elif self.current_planner is None:
+                rospy.logwarn_throttle(1, "Cannot get current planner")
+                r.sleep()
+                continue
+
+            rospy.loginfo_throttle(1, "=====")
+            task_type = self.search_task_from_node_id(
+                self.current_checkpoint_id, self.next_checkpoint_id
+            )
+            rospy.loginfo_throttle(1, f"task_type : {task_type}")
+            rospy.loginfo_throttle(1, f"planner : {self.current_planner}")
+            rospy.loginfo_throttle(
+                1, f"current_checkpoint : {self.current_checkpoint_id}"
+            )
+            rospy.loginfo_throttle(
+                1, f"next_checkpoint : {self.next_checkpoint_id}"
+            )
+
+            # update task
+            if prev_task_type != task_type:
+                rospy.logwarn(f"task updated : {task_type}")
+                # detect_line
+                if task_type == "detect_line" and self.USE_DETECT_WHITE_LINE:
+                    try:
+                        resp = self.stop_line_detector_client(True)
+                        rospy.logwarn(resp.message)
+                    except rospy.ServiceException as e:
+                        rospy.logwarn(e)
+                    self.use_point_follow_planner()
+                    self.target_velocity.linear.x = (
+                        self.detect_line_pfp_target_velocity
+                    )
+                else:
+                    try:
+                        resp = self.stop_line_detector_client(False)
+                        rospy.logwarn(resp.message)
+                    except rospy.ServiceException as e:
+                        rospy.logwarn(e)
+
+                # traffic_light
+                if task_type == "traffic_light":
+                    try:
+                        resp = self.traffic_light_detector_client(True)
+                        rospy.logwarn(resp.message)
+                    except rospy.ServiceException as e:
+                        rospy.logwarn(e)
+                    self.use_dwa_planner()
+                else:
+                    try:
+                        resp = self.traffic_light_detector_client(False)
+                        rospy.logwarn(resp.message)
+                    except rospy.ServiceException as e:
+                        rospy.logwarn(e)
+
+                # point_follow_planner
+                if task_type == "in_line":
+                    self.use_point_follow_planner()
+
+                # slow
+                if task_type == "slow":
+                    self.target_velocity.linear.x = self.slow_target_velocity
+
+                # no task
+                if task_type == "":
+                    self.use_dwa_planner()
+
+                # skip_mode
+                if task_type == "" and not self.is_stop_node(
+                    self.stop_list, self.next_checkpoint_id
+                ):
+                    self.skip_mode_flag.data = True
+                else:
+                    self.skip_mode_flag.data = False
+
+                # stop node
+                if self.is_stop_node(
+                    self.stop_list, self.current_checkpoint_id
+                ):
+                    try:
+                        resp = self.task_stop_client(True)
+                        rospy.logwarn(resp.message)
+                    except rospy.ServiceException as e:
+                        rospy.logwarn(e)
+                    del self.stop_list[0]
+
+                # recovery_mode
+                if task_type == "" or task_type == "slow":
+                    self.recovery_mode_flag.data = True
+                else:
+                    self.recovery_mode_flag.data = False
+
+            # publish
+            self.target_velocity_pub.publish(self.target_velocity)
+            self.finish_flag_pub.publish(self.finish_flag.data)
+            self.skip_mode_flag_pub.publish(self.skip_mode_flag.data)
+            self.recovery_mode_flag_pub.publish(self.recovery_mode_flag.data)
+
+            if self.finish_flag.data:
+                rospy.sleep(self.sleep_time_after_finish)
+
+            prev_task_type = task_type
+            self.finish_flag.data = False
+
             r.sleep()
 
     def load_task_from_yaml(self):
-        with open(self.TASK_LIST_PATH) as file:
-            task_data = yaml.safe_load(file)
-            self.get_task = True
+        while not rospy.is_shutdown():
+            task_data = None
+            try:
+                with open(self.TASK_LIST_PATH) as file:
+                    task_data = yaml.safe_load(file)
+                if task_data is not None:
+                    break
+            except Exception as e:
+                rospy.logerr_throttle(5.0, e)
+                rospy.sleep(1.0)
         return task_data
 
     def load_stop_list_from_yaml(self):
-        with open(self.STOP_LIST_PATH) as file:
-            stop_list_from_yaml = yaml.safe_load(file)
-            self.get_stop_list = True
-        stop_list = []
-        for count, stop in enumerate(stop_list_from_yaml["stop_list"]):
-            stop_list.append(stop["node_id"])
+        while not rospy.is_shutdown():
+            try:
+                with open(self.STOP_LIST_PATH) as file:
+                    stop_list_from_yaml = yaml.safe_load(file)
+                stop_list = []
+                for count, stop in enumerate(stop_list_from_yaml["stop_list"]):
+                    stop_list.append(stop["node_id"])
+                if len(stop_list) != 0:
+                    break
+            except Exception as e:
+                rospy.logerr_throttle(5.0, e)
+                rospy.sleep(1.0)
         return stop_list
 
     def current_checkpoint_id_callback(self, msg):
@@ -303,20 +316,15 @@ class TaskManager:
                     del self.stop_list[0]
 
     def search_task_from_node_id(self, node0_id, node1_id):
-        if self.get_task == True:
-            for count, task in enumerate(self.task_data["task"]):
-                if (
-                    task["edge"]["node0_id"] == node0_id
-                    and task["edge"]["node1_id"] == node1_id
-                ):
-                    return task["task_type"]
-            return ""
-        else:
-            return ""
+        for count, task in enumerate(self.task_data["task"]):
+            if (
+                task["edge"]["node0_id"] == node0_id
+                and task["edge"]["node1_id"] == node1_id
+            ):
+                return task["task_type"]
+        return ""
 
     def is_stop_node(self, stop_list, checkpoint_id):
-        if self.get_stop_list == False:
-            return False
         if len(stop_list) == 0:
             return False
         if stop_list[0] == checkpoint_id:
